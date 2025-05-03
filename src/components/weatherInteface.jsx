453da9ba37b69f230ff5cc2a0604900dca81c7d9
smile792 <shikhalishikhbabaev2705@gmail.com>
2025-05-03 19:10:02 +0300

    Изменение выбора города с кнопок на выпадающий список

--- src/components/weatherInteface.jsx
+++ src/components/weatherInteface.jsx
@@ -6,29 +6,34 @@ import { useState } from "react";
 
 export const Weatherinterface = ({ weather, setCity }) => {
   const [text, setText] = useState("");
+  const [value, setValue] = useState("");
   const changeText = (e) => {
     setText(e.target.value);
   };
   const onText = () => {
+    const isValid = /^[A-Za-z]+(?:[\s-][A-Za-z]+)*$/.test(text.trim());
+    if (!isValid) {
+      alert("Введите корректное название города (только буквы)");
+      return;
+    }
     setCity(text);
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setCity(e.target.value);
   };
   return (
     <>
       <div className="weather-interface">
-        <div className="weather-interface-btn">
-          <MyButton
-            children={"Georgievsk"}
-            onClick={() => setCity("Georgievsk")}
-          />
-          <MyButton
-            children={"Pyatigorsk"}
-            onClick={() => setCity("Pyatigorsk")}
-          />
-          <MyButton
-            children={"Zheleznovodsk"}
-            onClick={() => setCity("Zheleznovodsk")}
-          />
-        </div>
+        <select value={value} onChange={handleChange}>
+          <option disabled value="">
+            Выберите город
+          </option>
+          <option value="Pyatigorsk">Pyatigorsk</option>
+          <option value="Zheleznovodsk">Zheleznovodsk</option>
+          <option value="Georgievsk">Georgievsk</option>
+        </select>
         <div className="weather-interface-input">
           <MyInput
             placeholder={"Название города на английском"}
