c754f17596b7cbc80fe9198877183f7f84ab6a93
smile792 <shikhalishikhbabaev2705@gmail.com>
2025-05-04 16:21:25 +0300

    Добавление редиректа на главную страницу, если страницы не существует

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
+import { WeatherByLonLat } from "./page/WeatherByLonLat.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Routes>
+      <Route path="/weatherByLonLat" />
+      <Route path="/*" element={<Navigate to="/" replace />} />
+    </Routes>
     <App />
   </BrowserRouter>
 );
